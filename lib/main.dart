bad2bddc0e3d4e3b82da5323192e4e92d4579952
eman123455 <ek1223@fayoum.edu.eg>
2024-10-11 01:46:44 +0300

    splash view done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/splash/presentation/view/splash_view.dart';
+import 'package:bookly_app/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +12,11 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return  GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor
+      ),
       home: SplashView(),
     );
   }
