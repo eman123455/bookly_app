6e0a297db8431abceb39de494792f49355627cfa
eman123455 <ek1223@fayoum.edu.eg>
2024-10-11 23:44:58 +0300

    add custom listview item to home page

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/Custom_app_bar.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -6,13 +7,15 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 26,vertical: 40),
+    return const  Padding(
+      padding: EdgeInsets.symmetric(horizontal: 26,vertical: 40),
       child:   Column(
         children: [
-          CustomAppBar()
+           CustomAppBar(),
+          CustomListViewItem(),
         ],
       ),
     );
   }
 }
+
