f4e64a99cb86b1df0d38f221f33fbca73f092a4a
eman123455 <ek1223@fayoum.edu.eg>
2024-10-28 21:06:53 +0300

    add title and subtitle to book detalis view

--- lib/Features/home/presentation/views/widgets/book_detalis_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_detalis_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_detalis_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly_app/consts.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetalisViewBody extends StatelessWidget {
@@ -13,7 +15,24 @@ class BookDetalisViewBody extends StatelessWidget {
        Padding(
          padding: EdgeInsets.symmetric(horizontal: MediaQuery.sizeOf(context).width*0.19),
          child: const CustomBookImage(),
-       )
+       ),
+       const SizedBox(height: 39,),
+       Text('The Jungle Book',
+       style: Styles.textStyle30.copyWith(
+        fontFamily: kMontserratMedium
+       ),
+       ),
+      
+       const SizedBox(height: 5,),
+       Opacity(
+        opacity: 0.7,
+         child: Text('Rudyard Kipling',
+          style: Styles.textStyle18.copyWith(
+            fontStyle: FontStyle.italic
+          ),
+         ),
+       ),
+
       ],
     );
   }
