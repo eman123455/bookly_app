78e4eb49f237872b97ba224da0e2a67e8cd1af86
eman123455 <ek1223@fayoum.edu.eg>
2024-11-11 00:27:12 +0200

    implement  fetch new book method

--- lib/Features/home/data/repo/home_repo_implement.dart
+++ lib/Features/home/data/repo/home_repo_implement.dart
@@ -1,21 +1,30 @@
 import 'package:bookly_app/Features/home/data/models/bookmodel/bookmodel.dart';
 import 'package:bookly_app/Features/home/data/repo/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_server.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImplement implements HomeRepo{
+class HomeRepoImplement implements HomeRepo {
+  ApiServer apiServer = new ApiServer();
   @override
-  Future<Either<Failure, List<Bookmodel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<Bookmodel>>> fetchNewBook() async {
+    try {
+      var data = await apiServer.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+      List<Bookmodel> bookList = [];
+      for (var item in data['items']) {
+        bookList.add(Bookmodel.fromJson(item));
+      }
+
+      return right(bookList);
+    } on Exception catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<Bookmodel>>> fetchNewBook() {
+  Future<Either<Failure, List<Bookmodel>>> fetchFeaturedBooks() {
     // TODO: implement fetchNewBook
     throw UnimplementedError();
   }
-
-
-
 }
